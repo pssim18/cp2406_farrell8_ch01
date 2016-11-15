9efea807ef244585881be676e9b962e9c23c6188
pssim18 <p_s_sim@hotmail.com>
2016-11-15 14:25:20 +0800

    Task 5 added

---
+++ Task5/src/MovieQuote.java
@@ -0,0 +1,9 @@
+/**
+ * Created by pohsheng on 15/11/2016.
+ */
+public class MovieQuote
+{
+    public static void main(String[] args)
+    {System.out.println("You talkin to me?");
+    }
+}
---
+++ Task5/src/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+/**
+ * Created by pohsheng on 15/11/2016.
+ */
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("You talkin to me?");
+        System.out.println("Movie: Taxi Driver");
+        System.out.println("Character: Travis Bickle");
+        System.out.println("Year: 1976");
+    }
+}
